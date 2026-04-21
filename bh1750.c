0162c6c5313176de2bb85e710a0d3eca601cf05e
Yugong Ge <geyugong@gmail.com>
2026-04-21 19:22:22 +0800

    task01 completed(1750)

--- bh1750.c
+++ bh1750.c
@@ -7,7 +7,18 @@
 #include "bh1750.h"
 #include "i2c.h"
 
-#define I2C_MASTER_BUS_ID 1
+#ifndef CONFIG_I2C_MASTER_BUS_ID
+#define CONFIG_I2C_MASTER_BUS_ID        1
+#endif
+#ifndef CONFIG_I2C_SCL_MASTER_PIN
+#define CONFIG_I2C_SCL_MASTER_PIN       16
+#endif
+#ifndef CONFIG_I2C_SDA_MASTER_PIN
+#define CONFIG_I2C_SDA_MASTER_PIN       15
+#endif
+#ifndef CONFIG_I2C_MASTER_PIN_MODE
+#define CONFIG_I2C_MASTER_PIN_MODE      2
+#endif
 
 uint16_t bh1750_buff_H = 0;
 uint16_t bh1750_buff_L = 0;
@@ -18,7 +29,7 @@ void bh1750_SendCMD(uint8_t cmd)
     i2c_data_t data = {0};
     data.send_buf = buffer;
     data.send_len = sizeof(buffer);
-    errcode_t ret = uapi_i2c_master_write(I2C_MASTER_BUS_ID,
+    errcode_t ret = uapi_i2c_master_write(CONFIG_I2C_MASTER_BUS_ID,
                                           BH1750_ADDR >> 1, &data);
     if (ret != 0) {
         printf("BH1750:I2cWriteCMD(%02X) failed, %0X!\n", cmd, ret);
@@ -32,7 +43,7 @@ void bh1750_ReadData(void)
     i2c_data_t data;
     data.receive_len = sizeof(buffer);
     data.receive_buf = buffer;
-    errcode_t ret = uapi_i2c_master_read(I2C_MASTER_BUS_ID,
+    errcode_t ret = uapi_i2c_master_read(CONFIG_I2C_MASTER_BUS_ID,
                                          BH1750_ADDR >> 1, &data);
     if (ret != 0) {
         printf("BH1750:I2cRead(len:%d) failed, %0X!\n", data.receive_len, ret);
--- cw2015.c
+++ cw2015.c
@@ -7,7 +7,18 @@
 #include "cw2015.h"
 #include "i2c.h"
 
-#define I2C_MASTER_BUS_ID 1
+#ifndef CONFIG_I2C_MASTER_BUS_ID
+#define CONFIG_I2C_MASTER_BUS_ID        1
+#endif
+#ifndef CONFIG_I2C_SCL_MASTER_PIN
+#define CONFIG_I2C_SCL_MASTER_PIN       16
+#endif
+#ifndef CONFIG_I2C_SDA_MASTER_PIN
+#define CONFIG_I2C_SDA_MASTER_PIN       15
+#endif
+#ifndef CONFIG_I2C_MASTER_PIN_MODE
+#define CONFIG_I2C_MASTER_PIN_MODE      2
+#endif
 
 uint32_t cw2015_vecll_buff_H = 0;
 uint32_t cw2015_vecll_buff_L = 0;
@@ -18,7 +29,7 @@ void cw2015_SendREG(uint8_t reg, uint8_t reg_data)
     i2c_data_t data = {0};
     data.send_buf = buffer;
     data.send_len = sizeof(buffer);
-    errcode_t ret = uapi_i2c_master_write(I2C_MASTER_BUS_ID,
+    errcode_t ret = uapi_i2c_master_write(CONFIG_I2C_MASTER_BUS_ID,
                                           CW2015_ADDR >> 1, &data);
     if (ret != 0) {
         printf("CW2015:I2cWriteREG(%02X) failed, %0X!\n", reg, ret);
@@ -35,7 +46,7 @@ uint8_t cw2015_ReadREG(uint8_t reg)
     data.send_len = sizeof(send_buffer);
     data.receive_buf = read_buffer;
     data.receive_len = sizeof(read_buffer);
-    errcode_t ret = uapi_i2c_master_writeread(I2C_MASTER_BUS_ID,
+    errcode_t ret = uapi_i2c_master_writeread(CONFIG_I2C_MASTER_BUS_ID,
                                               CW2015_ADDR >> 1, &data);
     if (ret != 0) {
         printf("CW2015:I2cReadREG(%02X) failed, %0X!\n", reg, ret);
